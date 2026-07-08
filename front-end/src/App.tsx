e768bcf420627b52561340a4bb633f3d0cda1916
liviadfsilva <liviadfsilva@gmail.com>
2026-07-08 16:45:50 -0300

    feat: add react router

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,14 @@
-import Home from "./pages/Home/";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import PessoaDetalhes from "./pages/PessoaDetalhes";
 
 function App() {
-    return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/pessoas/:id" element={<PessoaDetalhes />} />
+    </Routes>
+  );
 }
 
 export default App;
